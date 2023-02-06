0a811bab2fcca9886db4098a8dc12c0cbe91d992
Menna <mennaelhosiny>
2023-02-06 22:43:29 +0200

    first commit

---
+++ task.js
@@ -0,0 +1,35 @@
+function div1(){
+    document.querySelector(".page1").style.display='block'
+    document.querySelector(".page2").style.display='none'
+    document.querySelector(".page3").style.display='none'
+    document.querySelector(".page4").style.display='none'
+
+
+
+
+}
+function div2(){
+    document.querySelector(".page1").style.display='none'
+    document.querySelector(".page2").style.display='block'
+    document.querySelector(".page3").style.display='none'
+    document.querySelector(".page4").style.display='none'
+
+
+
+}
+function div3(){
+    document.querySelector(".page1").style.display='none'
+    document.querySelector(".page2").style.display='none'
+    document.querySelector(".page3").style.display='block'
+    document.querySelector(".page4").style.display='none'
+
+
+
+}
+function div4(){
+    document.querySelector(".page1").style.display='block'
+    document.querySelector(".page2").style.display='block'
+    document.querySelector(".page3").style.display='block'
+
+
+}
